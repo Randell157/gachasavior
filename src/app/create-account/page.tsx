2e214a931130ae2b00664f10fa8758f86b06598a
Randell Lapid <randelljustinlapid@gmail.com>
2024-10-14 18:02:24 -0700

    added page and fixed erros
    Remove unused button imports

--- src/app/create-account/page.tsx
+++ src/app/create-account/page.tsx
@@ -1,5 +1,4 @@
 import Link from "next/link";
-import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
@@ -38,29 +37,7 @@ export default function Home() {
         </div>
       </header>
       <main className="flex-1">
-        <section className="w-full py-12 md:py-24 lg:py-32 xl:py-48">
-          <div className="container px-4 md:px-6 mx-auto">
-            <div className="flex flex-col items-center space-y-4 text-center">
-              <div className="space-y-2">
-                <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none">
-                  Welcome to Gacha Savior
-                </h1>
-                <p className="mx-auto max-w-[700px] text-gray-500 md:text-xl dark:text-gray-400">
-                  This is a website where you can organize your gacha account
-                  data in an organized way
-                </p>
-              </div>
-              <div className="space-x-4">
-                <Link href="login">
-                  <Button>Log In</Button>
-                </Link>
-                <Link href="create-account">
-                  <Button variant="outline">Create Account</Button>
-                </Link>
-              </div>
-            </div>
-          </div>
-        </section>
+        <h1>Create Account Page</h1>
       </main>
       <footer className="w-full py-6 border-t">
         <div className="container px-4 md:px-6 mx-auto flex flex-col sm:flex-row justify-between items-center">
