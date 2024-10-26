64cdc4088ae04eb05e0f52817f37a2d7883e51d2
Randell Lapid <randelljustinlapid@gmail.com>
2024-10-26 02:59:48 -0700

    fixed authprovider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={inter.className}>
+        <AuthProvider>{children}</AuthProvider>
+      </body>
     </html>
   );
 }
