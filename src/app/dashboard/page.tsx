833059b89c2c6290320e8cd85157cc2175912f2c
Randell Lapid <randelljustinlapid@gmail.com>
2024-10-26 02:27:29 -0700

    fixed firebase and added dashboard

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,65 @@
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <header className="px-4 lg:px-6 h-14 flex items-center justify-center">
+        <div className="container flex items-center justify-between">
+          <Link className="flex items-center justify-center" href="/">
+            <span className="font-bold">Gacha Savior</span>
+          </Link>
+          <nav className="flex gap-4 sm:gap-6">
+            <Link
+              className="text-sm font-medium hover:underline underline-offset-4"
+              href="/features"
+            >
+              Features
+            </Link>
+            <Link
+              className="text-sm font-medium hover:underline underline-offset-4"
+              href="/about"
+            >
+              About
+            </Link>
+            <Link
+              className="text-sm font-medium hover:underline underline-offset-4"
+              href="/contact"
+            >
+              Contact
+            </Link>
+            <Link
+              className="text-sm font-medium hover:underline underline-offset-4"
+              href="/login"
+            >
+              Login
+            </Link>
+          </nav>
+        </div>
+      </header>
+      <main className="flex-1">
+        
+      </main>
+      <footer className="w-full py-6 border-t">
+        <div className="container px-4 md:px-6 mx-auto flex flex-col sm:flex-row justify-between items-center">
+          <p className="text-xs text-gray-500 dark:text-gray-400">
+            © 2024 Gacha Savior. All rights reserved.
+          </p>
+          <nav className="flex gap-4 sm:gap-6 mt-4 sm:mt-0">
+            <Link
+              className="text-xs hover:underline underline-offset-4"
+              href="/terms"
+            >
+              Terms of Service
+            </Link>
+            <Link
+              className="text-xs hover:underline underline-offset-4"
+              href="/privacy"
+            >
+              Privacy
+            </Link>
+          </nav>
+        </div>
+      </footer>
+    </div>
+  );
+}
