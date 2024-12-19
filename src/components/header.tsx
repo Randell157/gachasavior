4cd12c4264e8d3391889fc7872c4e2087f4c2d10
Randell Lapid <randelljustinlapid@gmail.com>
2024-12-19 01:51:24 -0800

    Title leads to dashboard page (or login)

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <header className="px-4 lg:px-6 h-14 flex items-center justify-center">
       <div className="container flex items-center justify-between">
-        <Link className="flex items-center justify-center" href="/">
+        <Link className="flex items-center justify-center" href="/dashboard">
           <span className="font-bold">Gacha Savior</span>
         </Link>
         <nav className="flex gap-4 sm:gap-6">
