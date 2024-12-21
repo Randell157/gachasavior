f8f594a29ce57066446ac1b60b4b25bc2393b9c9
Randell Lapid <randelljustinlapid@gmail.com>
2024-12-20 20:20:58 -0800

    Update footer.tsx

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -7,20 +7,6 @@ export default function Footer() {
         <p className="text-xs text-gray-500 dark:text-gray-400">
           © 2024 Gacha Savior. All rights reserved.
         </p>
-        <nav className="flex gap-4 sm:gap-6 mt-4 sm:mt-0">
-          <Link
-            className="text-xs hover:underline underline-offset-4"
-            href="/terms"
-          >
-            Terms of Service
-          </Link>
-          <Link
-            className="text-xs hover:underline underline-offset-4"
-            href="/privacy"
-          >
-            Privacy
-          </Link>
-        </nav>
       </div>
     </footer>
   );
